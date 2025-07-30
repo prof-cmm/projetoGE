a5c3c50d644283e78cb80597a8585f82d953779b
prof-cmm <carolmontrezol@gmail.com>
2025-07-30 18:07:11 +0000

    listas e objetos parte2

--- script.js
+++ script.js
@@ -6,7 +6,30 @@ elemento.textContent = nomeUsuario
 
 inserirNome();
 
-let linguaguens = ["JavaScript", "Python", "C"];
+//Lista de linguagens escritas
 
-const item = document.querySelector("#lista");
-item.textContent = linguaguens[0];
+let linguagens = ["JavaScript", "Python", "C"];
+
+console.log(linguagens[0]); //JavaScript
+console.log(linguagens[1]); //Python
+console.log(linguagens[2]); //C
+
+const lista = document.querySelector("#lista");
+lista.textContent = linguagens[0];
+lista.textContent = linguagens[1];
+lista.textContent = linguagens[2];
+
+let aluno1 = {
+    nome: "Caroline",
+    idade: 46,
+    anoLetivo: "1º Série EM",
+    materiaFavoritas: ["Filosofia", "Arte", "Matemática", "Geografia"]
+
+}
+
+console.log(aluno1.nome);
+console.log(aluno1.materiaFavoritas[2]);
+
+const objeto = document.querySelector("#aluno");
+objeto.textContent = aluno1.nome
+objeto.textContent = aluno1.idade
